75d029287bcc881738ebbb588a5e7b0480291522
Arindam Karmakar <arindam.karmakar.96@gmail.com>
2020-09-15 23:08:26 +0530

    CRUD handling using MongoDB added

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
+const mongoose = require("mongoose");
 const genres = require("./routes/geners");
 const express = require("express");
 const app = express();
+
+mongoose
+	.connect("mongodb://localhost/vidlyDB", {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useFindAndModify: false,
+		useCreateIndex: true,
+	})
+	.then(() => {
+		console.info("Connected to MongoDB...");
+	})
+	.catch((error) => {
+		console.error("Failed to connect to MongoDB...", error);
+	});
 
 app.use(express.json()); // Middleware
 app.use("/api/genres", genres);
--- routes/geners.js
+++ routes/geners.js
@@ -1,47 +1,87 @@
 const express = require("express");
 const Joi = require("joi");
+const mongoose = require("mongoose");
 
 const router = express.Router();
 
-const genres = ["comedy", "drama", "action"];
+const genreSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: true,
+		minlength: 3,
+		maxlength: 255,
+	},
+});
+
+const Genre = new mongoose.model("Genre", genreSchema);
 
 // GET method
-router.get("/", (request, response) => {
+router.get("/", async (request, response) => {
+	const genres = await getGenres();
 	response.send(genres);
 });
 
 // POST method
-router.post("/", (request, response) => {
+router.post("/", async (request, response) => {
 	const { error } = validateGenre(request.body);
-	if (error) return response.status(400).send(error); // 400 Bad Request
+	if (error) return response.status(400).send(error); // 400 Bad Request;
 
-	const newGenre = request.body.name;
-	genres.push(newGenre);
-	response.send(genres);
+	const genre = await createGenre(request.body.name);
+
+	response.send(genre);
 });
 
 // PUT method
-router.put("/:name", (request, response) => {
+router.put("/:id", async (request, response) => {
 	const { error } = validateGenre(request.body);
 	if (error) return response.status(400).send(error); // 400 Bad Request
 
-	const name = request.params.name;
-	const index = genres.indexOf(name);
+	const genre = await updateGenre(request.params.id, request.body.name);
+	if (!genre) return response.status(404).send("Genre Not Found!"); //404 Not Found
 
-	if (!index) return response.status(404).send("Genre Not Found!"); //404 Not Found
-	genres[index] = request.body.name;
-	response.send(genres);
+	response.send(genre);
 });
 
 // DELETE method
-router.delete("/:name", (request, response) => {
-	const name = request.params.name;
-	const index = genres.indexOf(name);
+router.delete("/:id", async (request, response) => {
+	const genre = await removeGenre(request.params.id);
+	if (!genre) return response.status(404).send("Genre Not Found!"); //404 Not Found
 
-	if (!index) return response.status(404).send("Genre Not Found!"); //404 Not Found
-	genres.splice(index, 1);
-	response.send(genres);
+	response.send(genre);
 });
+
+async function getGenres() {
+	return await Genre.find().select("name").sort("name");
+}
+
+async function createGenre(genreName) {
+	let result;
+	const genre = new Genre({ name: genreName });
+
+	try {
+		result = await genre.save();
+	} catch (exception) {
+		console.error(exception);
+		result = null;
+	}
+	return result;
+}
+
+async function updateGenre(genreId, newGenreName) {
+	return await Genre.findByIdAndUpdate(
+		genreId,
+		{
+			name: newGenreName,
+		},
+		{
+			new: true,
+		}
+	);
+}
+
+async function removeGenre(genreId) {
+	return await Genre.findByIdAndRemove(genreId);
+}
 
 function validateGenre(genre) {
 	const schema = Joi.object({
